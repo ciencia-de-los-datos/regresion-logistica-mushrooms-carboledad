14f51c95df406ab730cf70241275e1b2bb9b8d0f
carboledad <101777132+carboledad@users.noreply.github.com>
2022-05-12 18:38:18 -0500

    Pregunta1_Ruta
    Pregunta1_Ruta

--- preguntas.py
+++ preguntas.py
@@ -64,9 +64,8 @@ def pregunta_01():
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    #df = df = pd.read_csv('mushrooms.csv')
-    df = df = pd.read_csv('/content/mushrooms.csv')
-
+    df = df = pd.read_csv('mushrooms.csv')
+    
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
     df.drop('veil_type', axis= 1, inplace = True)
